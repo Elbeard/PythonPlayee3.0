0a40122a39a46242eddbd58e4840d9a2a43f27d0
Semen Lapin <Elektrobeard@gmail.com>
2017-08-18 01:34:46 +0700

    Интерфейс заработал. Можно писать функционал.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-from PyQt5.QtWidgets import (QWidget, QPushButton,
+from PyQt5.QtWidgets import (QWidget, QPushButton, QFileDialog,
                              QMainWindow, QApplication, QStatusBar)
 from PyQt5.QtGui import QIcon
 
@@ -9,7 +9,9 @@ from PyQt5.QtGui import QIcon
 class View(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.controller = Controller()
         self.initUI()
+
 
     def initUI(self):
         #--------------------------- Кнопки -------------------------------#
@@ -25,6 +27,10 @@ class View(QMainWindow):
         self.statusBar
         self.statusBar().showMessage("Работает")
 
+        self.playButton.clicked.connect(self.controller.buttonClicked)
+        self.stopButton.clicked.connect(self.controller.buttonClicked)
+        self.openButton.clicked.connect(self.controller.openFile)
+
         #----------------------- Параметры главного окна ----------------------#
         self.setGeometry(80, 140, 400, 150)
         self.setWindowTitle('Питониум')
@@ -34,26 +40,17 @@ class View(QMainWindow):
 
 
 class Controller(QWidget):
-    def __init__(self):
-        self.view = View()
-
-        self.view.playButton.clicked.connect(self.buttonClicked)
-        self.view.stopButton.clicked.connect(self.buttonClicked)
-        self.view.openButton.clicked.connect(self.openFile)
 
     def buttonClicked(self):
         sender = self.sender()
-        self.view.statusBar().showMessage(sender.text() + ' was pressed')
+        mainWindow.statusBar().showMessage(sender.text() + ' was pressed')
 
     def openFile(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')[0]
-        #f = open(fname, 'r')
-        #with f:
-        #    data = f.read()
-        #    self.textEdit.setText(data)
+        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home')
+        mainWindow.statusBar().showMessage('File is opened')
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainWindow = Controller()
+    mainWindow = View()
     sys.exit(app.exec_())
